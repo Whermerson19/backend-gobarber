5ac7546963089cdf4bfa93c9e2acd779db446a83
Whermerson19 <whermersonc@gmail.com>
2020-10-18 16:04:55 -0300

    18/10/2020

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -5,23 +5,16 @@ import { classToClass } from 'class-transformer';
 
 import CreateUserService from '@modules/users/services/CreateUser/CreateUserService';
 
-interface userData {
-    name: string;
-    email: string;
-    password?: string;
-}
-
-
 export default class UsersController {
     public async create(request: Request, response: Response): Promise<Response> {
         const { name, email, password } = request.body;
 
         const createUser = container.resolve(CreateUserService);
     
-        const user: userData = await createUser.execute({
-            name,
-            email,
-            password
+        const user = await createUser.execute({
+          name,
+          email,
+          password,
         });
     
         return response.json(classToClass(user));
--- src/modules/users/services/SendForgotPasswordMail/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordMail/SendForgotPasswordEmailService.ts
@@ -44,7 +44,7 @@ export default class SendForgotPasswordEmailService {
                 file: forgotPasswordTemplate,
                 variables: {
                     name: user.name,
-                    link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`,
+                    link: `http://localhost:3000/reset-password?token=${token}`,
                 }
             }
         })
